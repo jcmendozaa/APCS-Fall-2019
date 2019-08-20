6ef56fec537529f9f72354c700fe59214f1383dc
APCS3rd <APCS3rd@MHS-LT9-15>
2019-08-20 10:23:53 -0700

    Hello World Complete

---
+++ hello world/src/HelloWorld.java
@@ -0,0 +1,12 @@
+// JC MENDOZA
+// August 19 2019
+// Prints "Hello World!"
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.print("Hello World!");
+	}
+
+}
