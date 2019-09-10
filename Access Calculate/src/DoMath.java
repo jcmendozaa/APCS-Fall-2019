18421e606eda893d3d007f5b113948f4b8cd6453
APCS3rd <APCS3rd@MHS-LT9-15>
2019-09-10 11:05:00 -0700

    hi

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -7,6 +7,18 @@ public class DoMath {
 
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(5));
+		System.out.println(Calculate.cube(2));
+		System.out.println(Calculate.average(4.5,5.5));
+		System.out.println(Calculate.average(2.5, 5.5, 4.5));
+		System.out.println(Calculate.toDegrees(5));
+		System.out.println(Calculate.toRadians(274));
+		System.out.println(Calculate.toImproperFrac(1, 5, 3));
+		System.out.println(Calculate.foil(2,3,6,-7,"n"));
+		System.out.println(Calculate.isDivisibleBy(2,71));
+		System.out.println(Calculate.absValue(-5.123));
+		System.out.println(Calculate.max(5.123, -2.55555555));
+		System.out.println(Calculate.max(3, 5, 100));
+		System.out.println(Calculate.min(46, 7));
 	}
 
 }
--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -3,7 +3,108 @@
  * Library of math functions.
  */
 public class Calculate {
+	// method square get the square number of the input
 	public static int square(int number) {
 		return number*number;
+	}
+	// method cube gets the cube number of the input
+	public static int cube(int integer) {
+		int cube = 0;
+		cube = integer*integer*integer;
+		return cube;
+	}
+	// method average takes average of two input numbers
+	public static double average(double number1, double number2) {
+		double average = (number1 + number2) / 2;
+		return average;
+	}
+	// method average takes average of three input numbers
+	public static double average(double number1, double number2, double number3) {
+		double answer = (number1 + number2 + number3) / 3;
+		return answer;
+	}
+	// method toDegrees converts radian to degree
+	public static double toDegrees(double radian) {
+		double degree;
+		degree = radian * (180 / 3.14159);
+		return degree;
+	}
+	// method toRadians converts degrees to radian
+	public static double toRadians(double degree) {
+		double radian;
+		radian = degree * (3.14159 / 180);
+		return radian;
+	}
+	// method discriminant returns the discriminant of a quadratic equation in standard form.
+	public static double discriminant(double input1, double input2, double input3) {
+		double discriminant;
+		discriminant = (input2 * input2) - (4 * input1 * input3);
+		return discriminant;   
+	}
+	public static String toImproperFrac(int whole, int numerator, int denominator) {
+		String answer;
+		answer = ((whole*denominator + numerator) + "/" + denominator);
+		return answer;
+	}
+	public static String toMixedNum(int numerator, int denominator) {
+		String answer;
+		answer = (numerator / denominator + " " + numerator % denominator + "/" + denominator);
+		return answer;
+	}
+	public static String foil(int input1, int input2, int input3, int input4, String letter) {
+		String a;
+		String b;
+		int c;
+		String combined;
+		a = (input1 * input3) + "n^2 + ";
+		b = (input2 * input3) + (input1 * input4) + "n + ";
+		c = (input2 * input4);
+		combined = a + b + c;
+		return combined;
+	}
+	public static boolean isDivisibleBy(int input1, int input2) {
+		if((input1 % input2 == 0) || (input2 % input1 == 0)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public static double absValue(double number) {
+		if(number < 0) {
+			number *= -1;
+		}
+		return number;
+	}
+	public static double max(double input1, double input2) {
+		if(input1 > input2) {
+			return input1;
+		} else {
+			return input2;
+		}
+	}
+	public static double max(double number1, double number2, double number3) {
+		double greaterNum;
+		if ((number1 > number2) && (number1 > number3)) {
+			greaterNum = number1;
+		} else {
+			if ((number2 > number1) && (number2 > number3)) {
+				greaterNum = number2;
+			} else {
+				greaterNum = number3;
+			}
+		  }
+		return greaterNum;
+	}
+	public static int min(int number1, int number2) {
+		int minimum;
+		if (number1 >= number2) {
+			minimum = number1;
+		} else {
+			minimum = number2;
+		  }
+		return minimum;
+	}
+	public static double round2(double input) {
+		
 	}
 }
